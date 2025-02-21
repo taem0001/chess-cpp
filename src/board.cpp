c630055d34952f242a9c56d3b3d034fea3c23dee
Taemur Baig <btaemur@yahoo.dk>
2025-02-21 08:22:17 +0100

    Fixed the write_fen function

--- src/board.cpp
+++ src/board.cpp
@@ -2,9 +2,10 @@
 #include "../include/piece.h"
 #include "../include/utils.h"
 #include <iostream>
-#define START_POS                                                              \
+#include <string>
+#define START_POS \
     "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R" // TODO: Add the rest of the
-                                                  // FEN-notation
+                                                        // FEN-notation
 
 Board::Board() { load_start_pos(board); }
 
@@ -15,102 +16,102 @@ void Board::load_pos(const char *fen, unsigned char *board) {
 
     while (*fen) {
         switch (*fen) {
-            case 'p':
-                board[index] = Piece::make_piece(Piece::pawn, Piece::black);
-                index++;
-                break;
-            case 'n':
-                board[index] = Piece::make_piece(Piece::knight, Piece::black);
-                index++;
-                break;
-            case 'b':
-                board[index] = Piece::make_piece(Piece::bishop, Piece::black);
-                index++;
-                break;
-            case 'r':
-                board[index] = Piece::make_piece(Piece::rook, Piece::black);
-                index++;
-                break;
-            case 'q':
-                board[index] = Piece::make_piece(Piece::queen, Piece::black);
-                index++;
-                break;
-            case 'k':
-                board[index] = Piece::make_piece(Piece::king, Piece::black);
-                index++;
-                break;
-            case 'P':
-                board[index] = Piece::make_piece(Piece::pawn, Piece::white);
-                index++;
-                break;
-            case 'N':
-                board[index] = Piece::make_piece(Piece::knight, Piece::white);
-                index++;
-                break;
-            case 'B':
-                board[index] = Piece::make_piece(Piece::bishop, Piece::white);
-                index++;
-                break;
-            case 'R':
-                board[index] = Piece::make_piece(Piece::rook, Piece::white);
-                index++;
-                break;
-            case 'Q':
-                board[index] = Piece::make_piece(Piece::queen, Piece::white);
-                index++;
-                break;
-            case 'K':
-                board[index] = Piece::make_piece(Piece::king, Piece::white);
-                index++;
-                break;
-            case '/':
-                break;
-            case '1':
-                board[index] = Piece::none;
-                index++;
-                break;
-            case '2':
-                for (int i = index; i < index + 2; i++) {
-                    board[i] = Piece::none;
-                }
-                index += 2;
-                break;
-            case '3':
-                for (int i = index; i < index + 3; i++) {
-                    board[i] = Piece::none;
-                }
-                index += 3;
-                break;
-            case '4':
-                for (int i = index; i < index + 4; i++) {
-                    board[i] = Piece::none;
-                }
-                index += 4;
-                break;
-            case '5':
-                for (int i = index; i < index + 5; i++) {
-                    board[i] = Piece::none;
-                }
-                index += 5;
-                break;
-            case '6':
-                for (int i = index; i < index + 6; i++) {
-                    board[i] = Piece::none;
-                }
-                index += 6;
-                break;
-            case '7':
-                for (int i = index; i < index + 7; i++) {
-                    board[i] = Piece::none;
-                }
-                index += 7;
-                break;
-            case '8':
-                for (int i = index; i < index + 8; i++) {
-                    board[i] = Piece::none;
-                }
-                index += 8;
-                break;
+        case 'p':
+            board[index] = Piece::make_piece(Piece::pawn, Piece::black);
+            index++;
+            break;
+        case 'n':
+            board[index] = Piece::make_piece(Piece::knight, Piece::black);
+            index++;
+            break;
+        case 'b':
+            board[index] = Piece::make_piece(Piece::bishop, Piece::black);
+            index++;
+            break;
+        case 'r':
+            board[index] = Piece::make_piece(Piece::rook, Piece::black);
+            index++;
+            break;
+        case 'q':
+            board[index] = Piece::make_piece(Piece::queen, Piece::black);
+            index++;
+            break;
+        case 'k':
+            board[index] = Piece::make_piece(Piece::king, Piece::black);
+            index++;
+            break;
+        case 'P':
+            board[index] = Piece::make_piece(Piece::pawn, Piece::white);
+            index++;
+            break;
+        case 'N':
+            board[index] = Piece::make_piece(Piece::knight, Piece::white);
+            index++;
+            break;
+        case 'B':
+            board[index] = Piece::make_piece(Piece::bishop, Piece::white);
+            index++;
+            break;
+        case 'R':
+            board[index] = Piece::make_piece(Piece::rook, Piece::white);
+            index++;
+            break;
+        case 'Q':
+            board[index] = Piece::make_piece(Piece::queen, Piece::white);
+            index++;
+            break;
+        case 'K':
+            board[index] = Piece::make_piece(Piece::king, Piece::white);
+            index++;
+            break;
+        case '/':
+            break;
+        case '1':
+            board[index] = Piece::none;
+            index++;
+            break;
+        case '2':
+            for (int i = index; i < index + 2; i++) {
+                board[i] = Piece::none;
+            }
+            index += 2;
+            break;
+        case '3':
+            for (int i = index; i < index + 3; i++) {
+                board[i] = Piece::none;
+            }
+            index += 3;
+            break;
+        case '4':
+            for (int i = index; i < index + 4; i++) {
+                board[i] = Piece::none;
+            }
+            index += 4;
+            break;
+        case '5':
+            for (int i = index; i < index + 5; i++) {
+                board[i] = Piece::none;
+            }
+            index += 5;
+            break;
+        case '6':
+            for (int i = index; i < index + 6; i++) {
+                board[i] = Piece::none;
+            }
+            index += 6;
+            break;
+        case '7':
+            for (int i = index; i < index + 7; i++) {
+                board[i] = Piece::none;
+            }
+            index += 7;
+            break;
+        case '8':
+            for (int i = index; i < index + 8; i++) {
+                board[i] = Piece::none;
+            }
+            index += 8;
+            break;
         }
 
         ++fen;
@@ -123,21 +124,20 @@ std::string Board::write_fen() {
     std::string res = "";
 
     while (index < 64) {
-
         switch (board[index]) {
-            case 0:
-                spacing++;
-                break;
-            default:
-                res += evaluate_fen_char(spacing, Piece::get_symbol(board[index]));
-                break;
-        }
-
-        if (spacing == 8) {
-            spacing = 0;
+        case 0:
+            spacing++;
+            break;
+        default:
+            res += evaluate_fen_char(spacing, Piece::get_symbol(board[index]));
+            break;
         }
 
         if (index % 8 == 7 && index != 63) {
+            if (spacing > 0) {
+                res += std::to_string(spacing);
+                spacing = 0;
+            }
             res += "/";
         }
 
