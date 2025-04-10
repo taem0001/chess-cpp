4f6d51b9cf665210af6eddad738cb7bc2486bf12
taem0001 <s235268@dtu.dk>
2025-04-11 01:55:08 +0200

    Working on bot

--- src/bot.cpp
+++ src/bot.cpp
@@ -1,6 +1,6 @@
 #include "../include/bot.h"
 
 u32 Bot::choose_move(std::vector<u32> &moves) {
-    int rand = std::rand() % (moves.size() + 1);
+    int rand = std::rand() % moves.size();
     return moves.at(rand);
 }
