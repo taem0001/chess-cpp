ab1ba18bedaf8646001d56a6ed64309f751518bb
Taemur Baig <s235268@dtu.dk>
2025-06-03 00:30:11 +0200

    Deleted unnecessary files

--- bot.ps1
+++
@@ -1,9 +0,0 @@
-.\build.ps1
-
-$exe = "build/Release/runBot.exe"
-if (Test-Path $exe) {
-    Write-Host "Running runBot..."
-    & $exe
-} else {
-    Write-Error "$exe not found."
-}
--- run.ps1
+++
@@ -1,9 +0,0 @@
-.\build.ps1
-
-$exe = "build/Release/runChess.exe"
-if (Test-Path $exe) {
-    Write-Host "Running runChess..."
-    & $exe
-} else {
-    Write-Error "$exe not found."
-}
